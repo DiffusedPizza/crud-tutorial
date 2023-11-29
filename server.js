b037ed4e51a4e5a6e778d97ae87257ff90e3263a
DiffusedPizza <rubenlizardi79@gmail.com>
2023-11-29 14:58:02 -0700

    send off

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 
+
+
+
 app.get('/', (req, res) => {
     res.sendFile( __dirname + '/index.html')
   })
+
+app.post('/quotes', (req, res) => {
+console.log('Hellooooooooooooooooo!')
+})
 
 app.listen(3000, function () {
     console.log('listening on 3000')
