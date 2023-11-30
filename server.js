3a871dba894257a2d492a9ac8b7dceb78498b65d
DiffusedPizza <rubenlizardi79@gmail.com>
2023-11-30 09:42:37 -0700

    changed order structure

--- server.js
+++ server.js
@@ -7,15 +7,31 @@ MongoClient.connect('mongodb+srv://rubenlizardi79:Znl3FhWl3a07N470@cluster0.mevh
 .then(client => {
 console.log('Connected to Database')
 const db = client.db('star-wars-quotes')
+const quotesCollection = db.collection('quotes')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+
+    db.collection('quotes')
+    .find()
+    .toArray()
+    .then(results => {
+      console.log(results)
+    })
+    .catch(error => console.error(error))
+    const cursor = db.collection('quotes').find()
+    console.log(cursor)
     res.sendFile( __dirname + '/index.html')
   });
 
 app.post('/quotes', (req, res) => {
-console.log(req.body)
+    quotesCollection
+    .insertOne(req.body)
+    .then(result => {
+        res.redirect('/')
+    })
+    .catch(error => console.error(error))
 });
 
 app.listen(3000, function () {
