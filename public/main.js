bd0e177421d4ae08a3d9df0eb8af6d712faa5828
DiffusedPizza <rubenlizardi79@gmail.com>
2023-11-30 15:21:12 -0700

    restructure

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 app.use(express.static('public'))
+app.use(bodyParser.json())
+
 
 // main.js
 const update = document.querySelector('#update-button')
@@ -6,5 +8,13 @@ const update = document.querySelector('#update-button')
 update.addEventListener('click', _ => {
     fetch('/quotes', {
         method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Darth Vader',
+            quote: 'I find your lack of faith disturbing.',
+          }),
       })
 })
+
+  
+
